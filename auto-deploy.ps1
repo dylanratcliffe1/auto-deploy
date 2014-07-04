7c43c5ff7a286ff8ed4e61b4e4cfbc47508a29f2
Dylan Ratcliffe <dylan.ratcliffe@smsmt.com>
2014-07-04 11:29:59 +1000

    Moved logging so that it only logs when it actually runs

--- auto-deploy.ps1
+++ auto-deploy.ps1
@@ -302,9 +302,8 @@ function Replace-Localhost($file) {
 function Replace-InFile($File, $SearchRegex, $ReplaceString) {
     # Changing localhost to localhost\MSSQL in config file
     (Get-Content $File.FullName) | 
-    Foreach-Object {$_ -replace $SearchRegex, $ReplaceString} | 
+    Foreach-Object {$_ -replace $SearchRegex, $ReplaceString; Log ("Replaced $SearchRegex with $ReplaceString in " + $File.FullName)} | 
     Set-Content $File.FullName
-    Log ("Replaced $SearchRegex with $ReplaceString in " + $File.FullName)
 }
 
 # Local LDAP needs to be setup for GCIS
